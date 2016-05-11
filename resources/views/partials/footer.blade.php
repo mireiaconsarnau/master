bcdf322d388d2e05966cc55ef5f25fd0531ccb05
root <mireiaconsarnau@gmail.com>
2016-05-11 17:45:03 +0200

    a

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -4,7 +4,7 @@
     <div class="pull-right hidden-xs">
         coursejar.com
     </div>
-    {{date('l jS \of F Y h:i:s A')}} - <i>"I'm sorry, Dave. I'm afraid I can't do that."</i>
+    {{date('l jS \of F Y h:i:s A')}}
     <!-- Default to the left -->
 
 </footer>
